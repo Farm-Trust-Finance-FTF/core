2746ca9766a13fedeed0492233d625f0f4dde6e2
MarsIfeanyi <marschalice@gmail.com>
2023-11-10 10:09:31 +0100

    Initial Commit

---
+++ contracts/Lending-and-Borrowing/library/Errors.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+library Errors {
+    error NoFundsLocked(address msgSender, bool isLocked);
+    error NoMessageReceived(); 
+    error IndexOutOfBound(uint256 providedIndex, uint256 maxIndex); of bounds.
+    error MessageIdNotExist(bytes32 messageId);
+    error NotEnoughBalance(uint256, uint256);
+    error NothingToWithdraw(); 
+    error FailedToWithdrawEth(address owner, uint256 value);
+    }
