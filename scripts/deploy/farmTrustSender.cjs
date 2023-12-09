198979801c7899f588221590ee6863047a16f36d
MarsIfeanyi <marschalice@gmail.com>
2023-12-09 23:13:44 +0100

    Add Sepolia ChainId

--- scripts/deploy/farmTrustSender.cjs
+++ scripts/deploy/farmTrustSender.cjs
@@ -2,8 +2,12 @@ const { ethers } = require("hardhat");
 const { verify } = require("../../utils/verify.cjs");
 const { network } = require("hardhat");
 
-const ROUTER_ADDRESS = "0x554472a2720e5e7d5d3c817529aba05eed5f82d8";
-const LINK_ADDRESS = "0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846";
+// const ROUTER_ADDRESS = "0x554472a2720e5e7d5d3c817529aba05eed5f82d8";
+// const LINK_ADDRESS = "0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846";
+
+// sepolia
+const ROUTER_ADDRESS = "0xd0daae2231e9cb96b94c8512223533293c3693bf";
+const LINK_ADDRESS = "0x779877A7B0D9E8603169DdbD7836e478b4624789";
 
 async function main() {
   const farmTrustSenderContract = await ethers.deployContract(
@@ -19,7 +23,7 @@ async function main() {
     `FarmTrustSender Contract deployed to: ${farmTrustSenderContract.address}`
   );
 
-  if (network.config.chainId === 43113 && process.env.SNOWTRACE_API_KEY) {
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
     console.log("Waiting for block confirmations...");
 
     await farmTrustSenderContract.deployTransaction.wait(6);
@@ -38,3 +42,9 @@ main().catch((error) => {
 // 0x2d4dC4aDA01Bb17B1c705dd5a96E4a4CE9C9782C
 
 // https://testnet.snowtrace.io/address/0x2d4dC4aDA01Bb17B1c705dd5a96E4a4CE9C9782C#code
+
+// sepolia
+
+// 0xd6aeDa0EcC051aAABd0fD171FF04a5035fFa8C9c
+
+// https://sepolia.etherscan.io/address/0xd6aeDa0EcC051aAABd0fD171FF04a5035fFa8C9c#code
