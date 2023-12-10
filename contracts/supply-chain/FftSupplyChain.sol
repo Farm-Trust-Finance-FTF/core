ec2c68f5f7cc96e29bb614608467741766a89d30
Gino <ginowine1@gmail.com>
2023-12-10 06:01:07 +0100

    create structs variables and contract

---
+++ contracts/supply-chain/FftSupplyChain.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+contract FtfSupplyChain {
+        uint32 public product_id = 0;   // Product ID
+    uint32 public participant_id = 0;   // Participant ID
+    uint32 public owner_id = 0;   // Ownership ID
+
+    struct product {
+        string modelNumber;
+        string partNumber;
+        string serialNumber;
+        address productOwner;
+        uint32 cost;
+        uint32 mfgTimeStamp;
+    }
+
+    mapping(uint32 => product) public products;
+
+    struct participant {
+        string userName;
+        string password;
+        string participantType;
+        address participantAddress;
+    }
+    mapping(uint32 => participant) public participants;
+
+    struct ownership {
+        uint32 productId;
+        uint32 ownerId;
+        uint32 trxTimeStamp;
+        address productOwner;
+    }
+}
+
